61d6fa82abfcaae0a0415ee932cf1f70e01d56e2
JoshBarajas21 <l19251092@gamadero.tecnm.mx>
2023-04-25 14:33:19 +0000

    Global Scope 🌐

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 //1. Importanto el modulo de Node
 //http
-import http from 'http';
+import http from 'http'; //este es para type: 'module'
+
+import path from 'path'; // para poder usar __dirname y __filename
+
+//este es para type commonjs
+//let http = require('http');
+
+/* Usando el scope global */
+global["__dirname"] = path.dirname(new URL(import.meta.url).pathname);
+global["__filename"] = path.join(path.basename(new URL(import.meta.url).pathname));
+
+//path.join es para hacerlo multiplataforma y corran en linux y windows
 
 //2. Crear el servidor
 //la siguiente constante es el carro listo
 const server = http.createServer( ( req, res )=> {
     //Toda la lógica del server
     // 1. Respondiendo al cliente (res)
-    res.write("Hello from the server ...");
+    res.write("Hello from the server ... \n");
+    res.write(`__dirname: ${__dirname} \n`);
+    res.write(`__filename: ${__filename} \n`);
     //2. Cerrar la conexión
     res.end();
 } ); //const porque no va a cambiar el valor
